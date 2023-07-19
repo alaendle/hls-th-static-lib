9e680e27541f64a674220dbb773cf91337bf29cc
Andreas Ländle <969523+alaendle@users.noreply.github.com>
2023-07-19 07:07:44 +0000

    Reproduce HLS issue.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,30 @@
 import Distribution.Simple
-main = defaultMain
+import qualified Distribution.PackageDescription as PD
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+import System.Directory
+import Data.Maybe
+
+main = defaultMainWithHooks simpleUserHooks { confHook = gatewayConfHook }
+
+gatewayConfHook :: (PD.GenericPackageDescription, PD.HookedBuildInfo) ->
+                  ConfigFlags ->
+                  IO LocalBuildInfo
+gatewayConfHook (description, buildInfo) flags = do
+    localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+    let packageDescription = localPkgDescr localBuildInfo
+        library = fromJust $ PD.library packageDescription
+        libraryBuildInfo = PD.libBuildInfo library
+        libPref = libdir $ absoluteInstallDirs packageDescription localBuildInfo NoCopyDest
+    dir <- getCurrentDirectory
+    print $ "Extend library search path to: " ++ libPref -- only for debugging
+    pure localBuildInfo {
+        localPkgDescr = packageDescription {
+            PD.library = Just $ library {
+                PD.libBuildInfo = libraryBuildInfo {
+                    PD.extraLibDirs = libPref : (dir ++ "/lib") :PD.extraLibDirs libraryBuildInfo
+                }
+            }
+        }
+    }
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Lib
     ( someFunc
     ) where
 
+import qualified Language.C.Inline as C
+
+C.include "<math.h>"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  x <- [C.exp| double{ cos(1) } |]
+  print x
